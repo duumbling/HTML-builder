18770320e8e36ed8628b2d0c1656d31d9a4db181
dumbling <romanskrt@yandex.ru>
2023-05-08 01:33:18 +0300

    feat: add 04-copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+// const { isFunction } = require("util");
 
+const copyRecursive = (src, dest) => {
+  //true if path exists
+  const exist = fs.existsSync(src);
+  //true if object is file or directory
+  const stats = exist && fs.statSync(src);
+  //true if object is direcrory
+  const isDirectory = stats && stats.isDirectory();
+  //if object exists and its a directory
+  if (isDirectory) {
+    //create destination directory if destination directory doesn't exist already
+    if (!fs.existsSync(dest)) fs.mkdirSync(dest);
+    //copy all files from start directory to destination directory
+    fs.readdirSync(src).forEach((childItemName) => {
+      copyRecursive(
+        path.join(src, childItemName),
+        path.join(dest, childItemName)
+      );
+    });
+    //if objext exists and its a file
+  } else {
+    //create destnation file if destinationn file doens't exist
+    if (!fs.existsSync(dest)) fs.copyFileSync(src, dest);
+  }
+};
+
+copyRecursive(
+  path.resolve(__dirname, "files"),
+  path.resolve(__dirname, "files-copy")
+);
