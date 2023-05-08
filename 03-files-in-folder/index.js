5536ec257acdc5861ce0a1dae83ea8360cf9a440
dumbling <romanskrt@yandex.ru>
2023-05-08 04:08:06 +0300

    fix: remove sync methods

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,14 +8,17 @@ fs.readdir(testFolder, (err, files) => {
   }
   files.forEach((file) => {
     let fullPath = path.resolve(__dirname, "secret-folder", `${file}`);
-    if (fs.lstatSync(fullPath).isFile()) {
-      console.log(
-        path.basename(fullPath).split(".")[0],
-        "-",
-        path.basename(fullPath).split(".")[1],
-        "-",
-        fs.statSync(fullPath).size / 1024 + "kb"
-      );
-    }
+    fs.stat(fullPath, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+
+      if (stats.isFile()) {
+        let fileName = file.split(".")[0];
+        let fileSize = stats.size;
+        let fileEx = file.split(".")[1];
+        console.log(`${fileName} - ${fileEx} - ${fileSize}b`);
+      }
+    });
   });
 });
